58e7bb36e26f4a7583503d9de63422221c97b4aa
DanielSkill <danskillington@gmail.com>
2019-06-04 11:32:23 +0100

    change models namespace

---
+++ app/Services/PersistDataService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class PersistDataService
+{
+    public function saveMatchData($match)
+    {
+
+    }
+
+    public function savePlayerData($player)
+    {
+
+    }
+}
