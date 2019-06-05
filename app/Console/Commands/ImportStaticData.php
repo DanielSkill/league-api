fae75c3650f48501c9ade88918c8229066a5c855
DanielSkill <danskillington@gmail.com>
2019-06-05 15:54:37 +0100

    create command to import static data

--- app/Console/Commands/ImportStaticData.php
+++ app/Console/Commands/ImportStaticData.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Support\DDragonAPI\DDragonDataApi;
+use Illuminate\Support\Facades\Storage;
 
 class ImportStaticData extends Command
 {
@@ -21,13 +23,20 @@ class ImportStaticData extends Command
     protected $description = 'Import/Update all the static data.';
 
     /**
+     * @var DDragonDataApi
+     */
+    protected $ddragonApi;
+
+    /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(DDragonDataApi $ddragonApi)
     {
         parent::__construct();
+
+        $this->ddragonApi = $ddragonApi;
     }
 
     /**
@@ -37,6 +46,18 @@ class ImportStaticData extends Command
      */
     public function handle()
     {
-        //
+        $data = [
+            'champions' => $this->ddragonApi->getChampions(),
+            'runes' => $this->ddragonApi->getRunes(),
+            'profileicons' => $this->ddragonApi->getProfileIcons(),
+            'items' => $this->ddragonApi->getItems(),
+            'maps' => $this->ddragonApi->getMaps(),
+            'summoners' => $this->ddragonApi->getSummonerSpells()
+        ];
+
+        // TODO: write all data into database (optional)
+        foreach ($data as $key => $item) {
+            Storage::disk('public')->put($key . '.json', json_encode($item));
+        }
     }
 }
